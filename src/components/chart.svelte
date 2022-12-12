9529b6097892cd876bbf0ae956a2264955558538
Liempo <gonzalesalec@gmail.com>
2022-12-12 18:09:07 +0800

    Add chart options to chart component

--- src/components/chart.svelte
+++ src/components/chart.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
 	import { browser } from '$app/environment'
 	import { onMount } from 'svelte'
-
-	const options = {
-		chart: {
-			type: 'bar'
-		},
-		series: [
-			{
-				name: 'sales',
-				data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-			}
-		],
-		xaxis: {
-			categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-		}
-	}
+	export let options: ApexCharts.ApexOptions
 
 	onMount(async () => {
 		if (!browser) return
