4a01d40f968127c0ca7619958a2bf192fef34d75
Liempo <gonzalesalec@gmail.com>
2023-01-08 19:14:57 +0800

    Fix chart id

--- src/components/chart.svelte
+++ src/components/chart.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { browser } from '$app/environment'
 	import { onMount } from 'svelte'
+	export let id: string
 	export let options: ApexCharts.ApexOptions
 
 	onMount(async () => {
 		if (!browser) return
 		const { default: ApexCharts } = await import('apexcharts')
-		const chart = new ApexCharts(document.querySelector('#chart'), options)
+		const chart = new ApexCharts(document.querySelector(`#${id}`), options)
 		chart.render()
 	})
 </script>
 
-<div id="chart" />
+<div {id} />
--- src/routes/dashboard/overview/+page.svelte
+++ src/routes/dashboard/overview/+page.svelte
@@ -2,8 +2,6 @@
 	import Chart from '@/components/chart.svelte'
 	import Header from '@/components/header.svelte'
 	import Icon from '@/components/icon.svelte'
-
-	export const ssr = false
 </script>
 
 <content>
@@ -39,4 +37,35 @@
 			</div>
 		</div>
 	</summary>
+
+	<!-- Grid for bigger cards -->
+	<preview class="grid grid-cols-2 gap-4 mt-4">
+		<!-- Sales by category -->
+		<div class="rounded-2xl drop-shadow-sm p-6 bg-white">
+			<h3 class="text-brand-primary text-xl font-semibold">Sales by Category</h3>
+			<Chart
+				id="sales-by-category"
+				options={{
+					series: [44, 55, 41, 17, 15],
+					chart: {
+						type: 'donut'
+					},
+					labels: ['Apparel', 'Electronics', 'Furniture', 'Groceries', 'Others']
+				}}
+			/>
+		</div>
+
+		<!-- Sales by location -->
+		<div class="rounded-2xl drop-shadow-sm p-6 bg-white">
+			<h3 class="text-brand-primary text-xl font-semibold">Sales by Location</h3>
+			<Chart
+				id="sales-by-location"
+				options={{
+					chart: {
+						type: 'bar'
+					}
+				}}
+			/>
+		</div>
+	</preview>
 </content>
