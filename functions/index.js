cf547110f8e6e207273dac9193264a1b3d99ec44
Liempo <gonzalesalec@gmail.com>
2023-01-20 09:39:04 +0800

    Create a cloud function to serve SSR content

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let sveltekitSSRServer
+exports.sveltekitSSR = functions
+	.region('us-central1')
+	.https.onRequest(async (request, response) => {
+		if (!sveltekitSSRServer) {
+			functions.logger.info('Initialising SvelteKit SSR entry')
+			sveltekitSSRServer = require('./sveltekitSSR/index').default
+			functions.logger.info('SvelteKit SSR entry initialised!')
+		}
+		functions.logger.info('Requested resource: ' + request.originalUrl)
+		return sveltekitSSRServer(request, response)
+	})
